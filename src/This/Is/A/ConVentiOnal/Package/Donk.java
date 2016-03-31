398316d819bb596236f612a8fa36277269e39e4f
hlaaftana <atadorukaydin@gmail.com>
2016-03-31 23:28:50 +0300

    fixed major issue (thanks @meew0)

--- src/This/Is/A/ConVentiOnal/Package/Donk.java
+++ src/This/Is/A/ConVentiOnal/Package/Donk.java
@@ -1,7 +1,19 @@
 package This.Is.A.ConVentiOnal.Package;
 
+import java.util.concurrent.*;
+
 public class Donk {
+	static ExecutorService inMemoryOfMeew0 = Executors.newFixedThreadPool(1);
+
 	public static void main(String donks[]){
-		System.out.println("donk");
+		inMemoryOfMeew0.submit(new Runnable(){
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				System.out.println("donk");
+			}
+
+		});
 	}
 }
