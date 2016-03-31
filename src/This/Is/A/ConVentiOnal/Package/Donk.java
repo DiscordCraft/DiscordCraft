1ed79c599e993eaa693ecd2f11c9680427840662
hlaaftana <atadorukaydin@gmail.com>
2016-03-31 23:31:23 +0300

    Added memes

--- src/This/Is/A/ConVentiOnal/Package/Donk.java
+++ src/This/Is/A/ConVentiOnal/Package/Donk.java
@@ -3,10 +3,10 @@ package This.Is.A.ConVentiOnal.Package;
 import java.util.concurrent.*;
 
 public class Donk {
-	static ExecutorService inMemoryOfMeew0 = Executors.newFixedThreadPool(1);
+	static ExecutorService inMemoryOfMeme0 = Executors.newFixedThreadPool(1);
 
 	public static void main(String donks[]){
-		inMemoryOfMeew0.submit(new Runnable(){
+		inMemoryOfMeme0.submit(new Runnable(){
 
 			@Override
 			public void run() {
